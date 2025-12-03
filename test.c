fb6ddac7c679e146f48460b056ef4553b82d9ac9
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-03 21:25:34 +0700

    test git commit -m test git commit -m test

--- test.c
+++ test.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main() {
-  // printf() จะแสดงข้อความที่อยู่ภายในเครื่องหมายคำพูด
-  printf("Hello, World!\\n");
-  return 0;
+ 
+    int n;
+ 
+    int i;
+
+    printf("ต้องการให้แสดง 'Hello World' กี่บรรทัด? (ป้อนตัวเลข n): ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("Hello World\n");
+    }
+
+    return 0;
 }
