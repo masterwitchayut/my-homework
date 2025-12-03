afc0b705a2f498c6ab2254150646c50ad876c8de
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-04 00:20:13 +0700

    lab3

---
+++ lab3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int *GetSet(int *num);
+
+int main()
+{
+    int *data;
+    int num;
+
+    data = GetSet(&num);
+
+    printf("N: %d\n", num);
+    if (num > 0)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            printf("%d ", data[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+int *GetSet(int *num)
+{
+
+    static int data_array[100];
+    int N;
+
+    printf("Enter N (max 100): ");
+    if (scanf("%d", &N) != 1 || N <= 0 || N > 100)
+    {
+        *num = 0;
+        return NULL;
+    }
+
+    printf("Enter %d integers:\n", N);
+    for (int i = 0; i < N; i++)
+    {
+        if (scanf("%d", &data_array[i]) != 1)
+        {
+            N = i;
+            break;
+        }
+    }
+
+    *num = N;
+
+    return data_array;
+}
