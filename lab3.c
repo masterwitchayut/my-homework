4126279b0e8e2008f7e2b9732be8f82d5d4ed868
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-04 22:46:11 +0700

    lab2

--- lab3.c
+++ lab3.c
@@ -10,6 +10,7 @@ int main()
     data = GetSet(&num);
 
     printf("N: %d\n", num);
+
     if (num > 0)
     {
         for (int i = 0; i < num; i++)
@@ -24,11 +25,11 @@ int main()
 
 int *GetSet(int *num)
 {
-
     static int data_array[100];
     int N;
 
     printf("Enter N (max 100): ");
+
     if (scanf("%d", &N) != 1 || N <= 0 || N > 100)
     {
         *num = 0;
