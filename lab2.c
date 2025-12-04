67b950c47bfa8cc8b37a6e4a45e177eb4416364b
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-04 21:12:42 +0700

    lab2

--- lab2.c
+++ lab2.c
@@ -9,7 +9,7 @@ int main()
 
     GetSet(data, &num);
 
-    printf("\nNumber of elements (N): %d\n", num);
+    printf("\n Number: %d\n", num);
     printf("Elements: ");
 
     for (int i = 0; i < num; i++)
@@ -25,12 +25,12 @@ void GetSet(int arr[], int *num)
 {
     int N;
 
-    printf("Enter number of elements (max 100): ");
+    printf("Enter number (max 100): ");
 
     if (scanf("%d", &N) != 1 || N <= 0 || N > 100)
     {
         *num = 0;
-        printf("Invalid or out of range N. Operation stopped.\n");
+        printf("Invalid or out of range N.\n");
         return;
     }
 
@@ -41,7 +41,7 @@ void GetSet(int arr[], int *num)
         printf("Element %d: ", i + 1);
         if (scanf("%d", &arr[i]) != 1)
         {
-            printf("Invalid input detected. Stopping input.\n");
+            printf("Invalid input detected.\n");
             N = i;
             break;
         }
