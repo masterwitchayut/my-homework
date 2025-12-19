9517579f1edbc17a7bd41ec96cbe319141fbc556
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-19 17:37:26 +0700

    1.5

---
+++ 1.5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    int evenSum = 0;
+    int oddSum = 0;
+
+    if (scanf("%d", &number) != 1) {
+        return 1;
+    }
+
+    while (number != 0) {
+
+        if (number % 2 == 0) {
+            evenSum += number;
+        } else {
+            oddSum += number;
+        }
+
+        if (scanf("%d", &number) != 1) {
+            break;
+        }
+    }
+
+    printf("Even Sum: %d\n", evenSum);
+    printf("Odd Sum: %d\n", oddSum);
+    return 0;
+}
