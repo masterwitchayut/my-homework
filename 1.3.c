1b3a32742e365855c039037f16db840a7b2dfb8c
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-19 17:40:16 +0700

    1.3

--- 1.3.c
+++ 1.3.c
@@ -1,35 +1,49 @@
 #include <stdio.h>
 
-int main() {
-    int num1, num2, operationCode, result;
+int main()
+{
+    int num1;
+    int num2;
+    int operationCode;
+    int result;
 
-    if (scanf("%d %d %d", &num1, &num2, &operationCode) != 3) {
+    if (scanf("%d %d %d", &num1, &num2, &operationCode) != 3)
+    {
         return 1;
     }
 
-    switch (operationCode) {
-        case 1:   // บวก
+    switch (operationCode)
+    {
+        case 1:
             result = num1 + num2;
-            printf("%d", result);
+            printf("%d\n", result);
             break;
 
-        case 2:   // ลบ
+        case 2:
             result = num1 - num2;
-            printf("%d", result);
+            printf("%d\n", result);
             break;
 
-        case 3:   // คูณ
+        case 3:
             result = num1 * num2;
-            printf("%d", result);
+            printf("%d\n", result);
             break;
 
-        case 4:   // หาร
-            result = num1 / num2;
-            printf("%d", result);
+        case 4:
+            if (num2 == 0)
+            {
+                printf("Invalid Operation\n");
+            }
+            else
+            {
+                result = num1 / num2;
+                printf("%d\n", result);
+            }
             break;
 
         default:
-            printf("Invalid Operation");
+            printf("Invalid Operation\n");
+            break;
     }
 
     return 0;
