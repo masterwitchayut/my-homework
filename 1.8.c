73a8e4acd2c1aa0d6a75cd838e385739680cfb88
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-19 22:04:19 +0700

    1.8

--- 1.8.c
+++ 1.8.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 
+#define WORK_START 8
+#define WORK_END 17
+#define HOUR_MIN 0
+#define HOUR_MAX 23
+
 int main() {
-    int dayCode, hour;
+    int dayCode;
+    int hour;
 
     if (scanf("%d %d", &dayCode, &hour) != 2) {
         return 1;
     }
 
+    /* ตรวจสอบช่วงของ hour ก่อน */
+    if (hour < HOUR_MIN || hour > HOUR_MAX) {
+        printf("Invalid Hour\n");
+        return 0;
+    }
+
     switch (dayCode) {
-        /* วันจันทร์ถึงศุกร์ */
+
+        /* วันทำงาน: จันทร์ - ศุกร์ */
         case 1:
         case 2:
         case 3:
         case 4:
         case 5:
-            if (hour >= 8 && hour <= 17) {
+            if (hour >= WORK_START && hour <= WORK_END) {
                 printf("System Running (Workday)\n");
             } else {
                 printf("System Idle (Off-hours)\n");
             }
             break;
 
-        /* วันเสาร์และอาทิตย์ */
+        /* วันหยุด: เสาร์ - อาทิตย์ */
         case 6:
         case 7:
             printf("Weekend Relax Mode\n");
