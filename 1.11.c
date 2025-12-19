597eb52692524d4b1ddd0a5de22a0223e0712957
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-19 23:22:21 +0700

    1.11

--- 1.11.c
+++ 1.11.c
@@ -3,39 +3,32 @@
 int main() {
     int categoryCode;
     float price_before_vat;
-    float vatRate;
-    float vatAmount;
-    float totalBill;
+    float totalBill = 0.0;
+    float vatAmount = 0.0;
+    float vatRate = 0.0;
 
-    vatRate = 0.0;
-    vatAmount = 0.0;
-    totalBill = 0.0;
-
+    /* รับข้อมูลราคาก่อนภาษี และรหัสหมวดหมู่ */
     if (scanf("%f %d", &price_before_vat, &categoryCode) != 2) {
         return 1;
     }
 
     /* กำหนดอัตรา VAT ตามหมวดหมู่ */
-    switch (categoryCode) {
-        case 1:
-            vatRate = 0.07;
-            break;
-
-        case 2:
-            vatRate = 0.00;
-            break;
-
-        case 3:
-            vatRate = 0.15;
-            break;
-
-        default:
-            printf("Invalid Category\n");
-            printf("Total Price: 0.00\n");
-            return 0;
+    if (categoryCode == 1) {
+        vatRate = 0.07;
+    }
+    else if (categoryCode == 2) {
+        vatRate = 0.00;
+    }
+    else if (categoryCode == 3) {
+        vatRate = 0.15;
+    }
+    else {
+        printf("Invalid Category\n");
+        printf("Total Price: 0.00\n");
+        return 0;
     }
 
-    /* คำนวณ VAT และราคารวม */
+    /* คำนวณ VAT และราคาสุทธิ */
     vatAmount = price_before_vat * vatRate;
     totalBill = price_before_vat + vatAmount;
 
