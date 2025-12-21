dd50b50b0db03a476869dfe88254922843e257b0
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-22 01:46:59 +0700

    3.3

---
+++ 3.3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int studentId;
+    float score;
+};
+
+int main() {
+    int N, i;
+    float totalScore = 0.0, average;
+
+    if (scanf("%d", &N) != 1) {
+        return 1;
+    }
+
+    struct Student students[N];
+
+    for (i = 0; i < N; i++) {
+        if (scanf("%d %f %s",
+                  &students[i].studentId,
+                  &students[i].score,
+                  students[i].name) != 3) {
+            return 1;
+        }
+        totalScore += students[i].score;
+    }
+
+    average = totalScore / N;
+
+    printf("Average Score: %.2f\n", average);
+
+    return 0;
+}
