bde8225ba39c6be863127e38a9f6d21d1b6ff631
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-22 11:51:38 +0700

    3.6

--- 3.6.c
+++ 3.6.c
@@ -5,6 +5,17 @@ struct Transaction {
     float amount;
 };
 
+void calculateSummary(int N, struct Transaction transactions[], float *totalIncome, float *totalExpense, float *netBalance) {
+    for (int i = 0; i < N; i++) {
+        if (transactions[i].amount >= 0.0) {
+            *totalIncome += transactions[i].amount;
+        } else {
+            *totalExpense += -transactions[i].amount;
+        }
+    }
+    *netBalance = *totalIncome - *totalExpense;
+}
+
 int main() {
     int N, i;
     float totalIncome = 0.0;
@@ -21,15 +32,9 @@ int main() {
         if (scanf("%f %s", &transactions[i].amount, transactions[i].description) != 2) {
             return 1;
         }
-
-        if (transactions[i].amount >= 0.0) {
-            totalIncome += transactions[i].amount;
-        } else {
-            totalExpense += -transactions[i].amount;
-        }
     }
 
-    netBalance = totalIncome - totalExpense;
+    calculateSummary(N, transactions, &totalIncome, &totalExpense, &netBalance);
 
     printf("Total Income: %.2f\n", totalIncome);
     printf("Total Expense: %.2f\n", totalExpense);
