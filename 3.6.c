0167b1bf7b01f6faaa995f031fa35217d826a7fa
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-22 11:38:56 +0700

    3.6

--- 3.6.c
+++ 3.6.c
@@ -11,33 +11,26 @@ int main() {
     float totalExpense = 0.0;
     float netBalance = 0.0;
 
-    // รับจำนวนธุรกรรม
     if (scanf("%d", &N) != 1) {
         return 1;
     }
 
     struct Transaction transactions[N];
 
-    // รับข้อมูลแต่ละธุรกรรม
     for (i = 0; i < N; i++) {
-        if (scanf("%f %s",
-                  &transactions[i].amount,
-                  transactions[i].description) != 2) {
+        if (scanf("%f %s", &transactions[i].amount, transactions[i].description) != 2) {
             return 1;
         }
 
-        // ตรวจสอบรายรับ / รายจ่าย
         if (transactions[i].amount >= 0.0) {
             totalIncome += transactions[i].amount;
         } else {
-            totalExpense += -transactions[i].amount; // Absolute Value
+            totalExpense += -transactions[i].amount;
         }
     }
 
-    // คำนวณยอดคงเหลือ
     netBalance = totalIncome - totalExpense;
 
-    // แสดงผลลัพธ์
     printf("Total Income: %.2f\n", totalIncome);
     printf("Total Expense: %.2f\n", totalExpense);
     printf("Net Balance: %.2f\n", netBalance);
