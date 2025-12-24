b5460cc6b42f960e34a8d5680968dcacfaff8e7b
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-24 23:13:51 +0700

    lab3.2

--- lab3.2.c
+++ lab3.2.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 
 struct student {
-    char name[ 20 ];
+    char name[20];
     int age;
     char sex;
     float gpa;
 };
 
-void upgrade( struct student *child ) {
-    if (child->sex == 'M') {
-        child->gpa = child->gpa * 1.10;
-    } else if (child->sex == 'F') {
-        child->gpa = child->gpa * 1.20;
-    }
-}
+void upgrade(struct student *child);
 
 int main() {
     struct student aboy;
+
     aboy.sex = 'M';
     aboy.gpa = 3.00;
 
-    // ส่ง address ของ aboy เพื่อให้ฟังก์ชันสามารถแก้ไขค่าใน struct ได้
-    upgrade( &aboy );
+    upgrade(&aboy);
 
-    printf( "%.2f", aboy.gpa );
+    printf("%.2f", aboy.gpa);
 
     return 0;
+}
+
+void upgrade(struct student *child) {
+    if (child->sex == 'M') {
+        child->gpa = child->gpa + (child->gpa * 0.10);
+    } else if (child->sex == 'F') {
+        child->gpa = child->gpa + (child->gpa * 0.20);
+    }
 }
