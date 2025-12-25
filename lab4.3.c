e2f09223b691f30ed4070fe6b6e885be4ce73642
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-25 22:29:18 +0700

    lab4.3

---
+++ lab4.3.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+struct studentNode {
+    char name[ 20 ] ;
+    int age ;
+    char sex ;
+    float gpa ;
+    struct studentNode *next ;
+} ;
+
+struct studentNode pool[ 10 ] ;
+int p_idx = 0 ;
+
+void saveNode( struct studentNode *child, char n[], int a, char s, float g ) ;
+void GoNext1( struct studentNode *walk ) ;
+
+int main() {
+    struct studentNode *start, *now1, **now2 ;
+    
+    start = &pool[ p_idx++ ] ;
+    saveNode( start, "one", 6, 'M', 3.11 ) ;
+
+    start->next = &pool[ p_idx++ ] ;
+    saveNode( start->next, "two", 8, 'F', 3.22 ) ;
+
+    start->next->next = &pool[ p_idx++ ] ;
+    saveNode( start->next->next, "three", 10, 'M', 3.33 ) ;
+
+    start->next->next->next = &pool[ p_idx++ ] ;
+    saveNode( start->next->next->next, "four", 12, 'F', 3.44 ) ;
+    start->next->next->next->next = NULL ;
+
+    now1 = start ;
+    now2 = &start ;
+
+    GoNext1( now1 ) ;
+    printf( "%s ", now1->name ) ;
+
+    return 0 ;
+}
+
+void saveNode( struct studentNode *child, char n[], int a, char s, float g ) {
+    int i = 0 ;
+    while ( n[ i ] != '\0' && i < 19 ) {
+        child->name[ i ] = n[ i ] ;
+        i++ ;
+    }
+    child->name[ i ] = '\0' ;
+    child->age = a ;
+    child->sex = s ;
+    child->gpa = g ;
+}
+
+void GoNext1( struct studentNode *walk ) {
+    if ( walk != NULL && walk->next != NULL ) {
+        walk = walk->next ;
+        printf( "%s ", walk->name ) ;
+    }
+}
