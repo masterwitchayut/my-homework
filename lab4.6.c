39edab030d3d5e561e6d1e3de6338dcf2a43ffd1
masterwitchayut <masterwitchayut136@gmail.com>
2025-12-26 09:45:10 +0700

    lab4.6

--- lab4.6.c
+++ lab4.6.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-// สร้าง Macro เพื่อให้ภาษา C รู้จักคำว่า new และทำงานเหมือน C++
-#define new (struct studentNode*)malloc(sizeof(struct studentNode))
-
-// เนื่องจากไม่ใช้ stdlib.h จึงต้องประกาศ prototype ของ malloc เองเพื่อไม่ให้ error
-void* malloc(size_t size);
-
 struct studentNode {
     char name[ 20 ];
     int age;
@@ -14,36 +8,34 @@ struct studentNode {
     struct studentNode *next;
 };
 
-void saveNode( struct studentNode *child, char n[], int a, char s, float g );
-void GoNext2( struct studentNode **walk );
+void SaveNode( struct studentNode *child, char n[], int a, char s, float g );
+void GoNext2( struct studentNode ***walk );
 
 int main() {
     struct studentNode *start, *now1, **now2;
-    
-    // บรรทัดเหล่านี้จะเขียนตาม Pre-code เป๊ะๆ และไม่ Error แล้ว
     start = new struct studentNode;
-    saveNode( start, "one", 6, 'M', 3.11 );
+    SaveNode( start, "one", 6, 'M', 3.11 );
 
     start->next = new struct studentNode;
-    saveNode( start->next, "two", 8, 'F', 3.22 );
+    SaveNode( start->next, "two", 8, 'F', 3.22 );
 
     start->next->next = new struct studentNode;
-    saveNode( start->next->next, "three", 10, 'M', 3.33 );
+    SaveNode( start->next->next, "three", 10, 'M', 3.33 );
 
     start->next->next->next = new struct studentNode;
-    saveNode( start->next->next->next, "four", 12, 'F', 3.44 );
+    SaveNode( start->next->next->next, "four", 12, 'F', 3.44 );
+    start->next->next->next->next = NULL;
 
     now1 = start;
     now2 = &start;
 
-    GoNext2( now2 );
+    GoNext2( &now2 );
     printf( "%s ", (*now2)->name );
 
     return 0;
 }
 
-void saveNode( struct studentNode *child, char n[], int a, char s, float g ) {
-    // การ copy string แบบ manual เพื่อเลี่ยง string.h
+void SaveNode( struct studentNode *child, char n[], int a, char s, float g ) {
     int i = 0;
     while (n[i] != '\0' && i < 19) {
         child->name[i] = n[i];
@@ -57,8 +49,9 @@ void saveNode( struct studentNode *child, char n[], int a, char s, float g ) {
     child->next = NULL;
 }
 
-void GoNext2( struct studentNode **walk ) {
-    if (*walk != NULL && (*walk)->next != NULL) {
-        *walk = (*walk)->next;
+void GoNext2( struct studentNode ***walk ) {
+    if (**walk != NULL && (**walk)->next != NULL) {
+        **walk = (**walk)->next;
+        printf("%s %d %c %.2f\n", (**walk)->name, (**walk)->age, (**walk)->sex, (**walk)->gpa);
     }
 }
