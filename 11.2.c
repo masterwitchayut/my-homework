865968feade385bd3015b720fc02ca30e8e791ad
Mean <masterwitchayut136@gmail.com>
2026-03-07 12:13:13 +0700

    11.2

---
+++ 11.2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+void mergesort(int t[], int k);
+void merge(int *u, int m, int *v, int n, int *t);
+
+int main() {
+    int data[7] = { 4, 6, 1, 2, 5, 1, 8 };
+    int n = 7;
+    mergesort(data, n);
+    for (int i = 0; i < n; i++) printf("%d ", data[i]);
+    return 0;
+}
+
+void mergesort(int t[], int k) {
+    if (k > 1) {
+        int m = k / 2;
+        int n = k - m;
+        int u[m], v[n];
+
+        for (int i = 0; i < m; i++) {
+            u[i] = t[i];
+        }
+        for (int i = 0; i < n; i++) {
+            v[i] = t[m + i];
+        }
+
+        mergesort(u, m);
+        mergesort(v, n);
+        merge(u, m, v, n, t);
+    }
+}
+
+void merge(int *u, int m, int *v, int n, int *t) {
+    int i = 0, j = 0, l = 0;
+    while (i < m && j < n) {
+        if (u[i] <= v[j]) {
+            t[l] = u[i];
+            i++;
+        } else {
+            t[l] = v[j];
+            j++;
+        }
+        l++;
+    }
+    while (i < m) {
+        t[l] = u[i];
+        i++;
+        l++;
+    }
+    while (j < n) {
+        t[l] = v[j];
+        j++;
+        l++;
+    }
+}
